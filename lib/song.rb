5cca1471398f33bf8636f47051e86119df80a9a9
Casey Tansey <ctansey12600@outlook.com>
2022-09-10 20:14:46 -0400

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,31 @@
+class Song
+  attr_accessor :name, :artist, :genre
+  @@count = 0
+  @@genres = []
+  @@artists = []
+  def initialize(name, artist, genre)
+    @@count += 1
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@artists << artist
+    @@genres << genre
+    
+  end
+  def self.count
+    @@count
+  end
+  def self.genres
+    @@genres.uniq
+  end
+  def self.artists
+    @@artists.uniq
+  end
+  def self.genre_count
+    @@genres.tally
+  end
+  def self.artist_count
+    @@artists.tally
+  end
+end
+
